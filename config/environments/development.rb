8d0423a9d3dbf64ac396e84913d2e3bc4f7c9404
okamotchan <m.okamotchan@gmail.com>
2018-12-22 21:45:51 +0900

    smtp

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,6 +62,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
+  #simple mail transfer protocol
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
