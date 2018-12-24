f2e1a2cf8f35bd763c468d628321aa85ca7178e3
okamotchan <m.okamotchan@gmail.com>
2018-12-24 16:21:59 +0900

    devise for 以下にrouting追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {sessions: 'users/sessions'}
+  devise_for :users, controllers: {sessions: 'users/sessions', registrations: 'users/registrations', passwords: 'users/passwords', omniauth_callbacks: 'users/omniauth_callbacks'}
+  resources :users, :only ['index', 'show']
   root 'pages#index'
   get 'pages/show'
   root to: 'rooms#show'
