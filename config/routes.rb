ecb248ec036bbf23e0a8e3dc36ae0fb07c4c2b23
okamotchan <m.okamotchan@gmail.com>
2019-01-04 03:18:24 +0900

    sign up

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   root 'pages#index'
   get 'pages/show'
   root to: 'rooms#show'
-  #deviseのルーティング指定はこれでいいのか？
   get 'rooms/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount ActionCable.server => '/cable'
